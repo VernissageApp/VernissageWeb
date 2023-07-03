09f5bfdbc7ae7679ad3f752a4fe1cd98ba93704b
Marcin Czachurski <mczachurski@icloud.com>
2023-07-03 19:14:31 +0200

    Profile page

--- src/app/pages/profile/profile.page.ts
+++ src/app/pages/profile/profile.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { User } from 'src/app/models/user';
+import { AuthorizationService } from 'src/app/services/authorization/authorization.service';
+import { UsersService } from 'src/app/services/http/users.service';
 import { fadeInAnimation } from "../../animations/fade-in.animation";
 
 @Component({
@@ -8,4 +11,13 @@ import { fadeInAnimation } from "../../animations/fade-in.animation";
     animations: fadeInAnimation
 })
 export class ProfilePage {
+    user?: User;
+
+    constructor(private authorizationService: AuthorizationService, private usersService: UsersService) {
+    }
+
+    async ngOnInit(): Promise<void> {
+        const userFromToken = this.authorizationService.getUser();
+        this.user = await this.usersService.profile(userFromToken?.userName ?? '');
+    }
 }
