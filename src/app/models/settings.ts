67b5b938f630cfeb320d7b4b47829399b43171f9
Marcin Czachurski <mczachurski@icloud.com>
2023-12-18 19:19:11 +0100

    Add settings

--- src/app/models/settings.ts
+++ src/app/models/settings.ts
@@ -11,6 +11,9 @@ export class Settings {
     
     public corsOrigin = '';
     public maximumNumberOfInvitations = 0;
+    public maxCharacters = 500;
+    public maxMediaAttachments = 4;
+    public imageSizeLimit = 10_485_760;
     
     public emailHostname = '';
     public emailPort = 0;
