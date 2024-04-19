b0a1a766c18d9d5318ac7b9ae7a59cfd384000e4
Marcin Czachurski <mczachurski@icloud.com>
2024-04-19 15:40:53 +0200

    #27 Fix images layout with blurehash placeholders

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,26 @@
-import { Component, OnDestroy, AfterViewInit, ChangeDetectorRef } from '@angular/core';
+import { Component, OnDestroy, AfterViewInit, ChangeDetectorRef, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
 
 import { LoadingService } from './services/common/loading.service';
+import { RoutingStateService } from './services/common/routing-state.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnDestroy, AfterViewInit {
+export class AppComponent implements OnInit, OnDestroy, AfterViewInit {
   showLoader = false;
   loadingStateChangesSubscription?: Subscription;
 
   constructor(
     private loadingService: LoadingService,
+    private routingStateService: RoutingStateService,
     private changeDetectorRef: ChangeDetectorRef) {
+  }
+
+  ngOnInit(): void {
+    this.routingStateService.startRoutingListener();
   }
 
   ngAfterViewInit(): void {
