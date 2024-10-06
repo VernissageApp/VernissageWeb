41d42571b8fe9de446a613b91d6f2fa14408050b
Marcin Czachurski <mczachurski@icloud.com>
2024-10-06 10:13:48 +0200

    Add URL to mastodon account

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ import { LoadingService } from './services/common/loading.service';
 import { RoutingStateService } from './services/common/routing-state.service';
 import { DOCUMENT } from '@angular/common';
 import { SsrCookieService } from './services/common/ssr-cookie.service';
+import { SettingsService } from './services/http/settings.service';
 
 @Component({
     selector: 'app-root',
@@ -19,6 +20,7 @@ export class AppComponent implements OnInit, OnDestroy, AfterViewInit {
         @Inject(DOCUMENT) private documentRef: Document,
         private loadingService: LoadingService,
         private cookieService: SsrCookieService,
+        private settingsService: SettingsService,
         private routingStateService: RoutingStateService,
         private changeDetectorRef: ChangeDetectorRef) {
     }
@@ -32,6 +34,11 @@ export class AppComponent implements OnInit, OnDestroy, AfterViewInit {
             body?.classList.add('dark-theme');
             this.documentRef.querySelector("meta[name='theme-color']")?.setAttribute("content", "#303030");
         }
+
+        const internalMastodonUrl = this.settingsService.publicSettings?.mastodonUrl ?? '';
+        if (internalMastodonUrl.length > 0) {
+            this.createMastodonLink(internalMastodonUrl);
+        }
     }
 
     ngAfterViewInit(): void {
@@ -44,5 +51,13 @@ export class AppComponent implements OnInit, OnDestroy, AfterViewInit {
 
     ngOnDestroy(): void {
         this.loadingStateChangesSubscription?.unsubscribe();
+    }
+
+    private createMastodonLink(url: string): void {
+        const link: HTMLLinkElement = this.documentRef.createElement('link');
+        link.setAttribute('href', url);
+        link.setAttribute('rel', 'me');
+
+        this.documentRef.head.appendChild(link);
     }
 }
