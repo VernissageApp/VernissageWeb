3976bedf96ff0d756cc8610b134e80a0c8cef90f
Marcin Czachurski <mczachurski@icloud.com>
2024-11-10 15:55:31 +0100

    Add security headears

--- server.ts
+++ server.ts
@@ -1,6 +1,7 @@
 import { APP_BASE_HREF } from '@angular/common';
 import { CommonEngine } from '@angular/ssr';
 import express from 'express';
+import helmet from 'helmet';
 import { fileURLToPath } from 'node:url';
 import { dirname, join, resolve } from 'node:path';
 import AppServerModule from './src/main.server';
@@ -8,55 +9,94 @@ import { REQUEST, RESPONSE } from 'express.tokens';
 
 // The Express app is exported so that it can be used by serverless Functions.
 export function app(): express.Express {
-  const server = express();
-  const serverDistFolder = dirname(fileURLToPath(import.meta.url));
-  const browserDistFolder = resolve(serverDistFolder, '../browser');
-  const indexHtml = join(serverDistFolder, 'index.server.html');
+    const server = express();
 
-  const commonEngine = new CommonEngine();
+    const cspImg = process.env['VERNISSAGE_CSP_IMG'] || undefined;
+    if (cspImg) {
+        server.use(
+            helmet({
+                contentSecurityPolicy: {
+                    useDefaults: false,
+                    directives: {
+                        baseUri: ["'self'"],
+                        defaultSrc: ["'none'"],
+                        connectSrc: ["'self'"],
+                        formAction: ["'self'"],
+                        objectSrc: ["'none'"],
+                        frameAncestors: ["'none'"],
+                        fontSrc: ["'self'", "https://fonts.gstatic.com"],
+                        imgSrc: ["'self'","data:", "blob:", cspImg],
+                        scriptSrc: ["'self'", "'unsafe-inline'"],
+                        scriptSrcAttr: ["'self'", "'unsafe-inline'"],
+                        styleSrc: ["'self'", "'unsafe-inline'"],
+                        manifestSrc: ["'self'"],
+                        blockAllMixedContent: [],
+                        upgradeInsecureRequests: [],
+                    },
+                },
+                referrerPolicy: {
+                    policy: "same-origin",
+                },
+                xContentTypeOptions: false,
+                strictTransportSecurity: {
+                    maxAge: 31557600,
+                },
+                xFrameOptions: { action: "deny" }
+            }),
+        );
+    }
 
-  server.set('view engine', 'html');
-  server.set('views', browserDistFolder);
+    const serverDistFolder = dirname(fileURLToPath(import.meta.url));
+    const browserDistFolder = resolve(serverDistFolder, '../browser');
+    const indexHtml = join(serverDistFolder, 'index.server.html');
 
-  // Example Express Rest API endpoints
-  // server.get('/api/**', (req, res) => { });
-  // Serve static files from /browser
-  server.get('**', express.static(browserDistFolder, {
-    maxAge: '1y',
-    index: 'index.html',
-  }));
+    const commonEngine = new CommonEngine();
 
-  // All regular routes use the Angular engine
-  server.get('**', (req, res, next) => {
-    const { protocol, originalUrl, baseUrl, headers } = req;
+    server.set('view engine', 'html');
+    server.set('views', browserDistFolder);
 
-    commonEngine
-      .render({
-        bootstrap: AppServerModule,
-        documentFilePath: indexHtml,
-        url: `${protocol}://${headers.host}${originalUrl}`,
-        publicPath: browserDistFolder,
-        providers: [
-          { provide: APP_BASE_HREF, useValue: baseUrl },
-          { provide: REQUEST, useValue: req },
-          { provide: RESPONSE, useValue: res }
-        ],
-      })
-      .then((html) => res.send(html))
-      .catch((err) => next(err));
-  });
+    // Example Express Rest API endpoints
+    // server.get('/api/**', (req, res) => { });
+    // Serve static files from /browser
+    server.get('**', express.static(browserDistFolder, {
+        maxAge: '1y',
+        index: 'index.html',
+    }));
 
-  return server;
-}
+    // All regular routes use the Angular engine
+    server.get('**', (req, res, next) => {
+        const { protocol, originalUrl, baseUrl, headers } = req;
+
+        commonEngine
+            .render({
+                bootstrap: AppServerModule,
+                documentFilePath: indexHtml,
+                url: `${protocol}://${headers.host}${originalUrl}`,
+                publicPath: browserDistFolder,
+                providers: [
+                    { provide: APP_BASE_HREF, useValue: baseUrl },
+                    { provide: REQUEST, useValue: req },
+                    { provide: RESPONSE, useValue: res }
+                ],
+            })
+            .then((html) => {
+                res.set('Cache-Control', 'private, no-store, max-age=0');
+                return res.send(html)
+            })
+            .catch((err) => next(err));
+      });
+
+      return server;
+  }
 
 function run(): void {
-  const port = process.env['PORT'] || 8080;
+    const port = 8081; // process.env['PORT'] || 8081;
 
-  // Start up the Node server
-  const server = app();
-  server.listen(port, () => {
-    console.log(`Node Express server listening on http://localhost:${port}`);
-  });
+    // Start up the Node server
+    const server = app();
+    server.listen(port, () => {
+        console.log(`Node Express server listening on http://localhost:${port}`);
+    });
 }
 
-run();
+run();  
