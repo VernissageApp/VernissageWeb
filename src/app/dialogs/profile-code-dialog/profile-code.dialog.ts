092e92e6ecfd1deb5087d12cadedb5163b5a9049
Marcin Czachurski <mczachurski@icloud.com>
2025-02-04 13:33:43 +0100

    #223 Fix QR code redirection from profile

--- src/app/dialogs/profile-code-dialog/profile-code.dialog.ts
+++ src/app/dialogs/profile-code-dialog/profile-code.dialog.ts
@@ -1,7 +1,6 @@
 import { ChangeDetectionStrategy, Component, Inject, OnInit, signal } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { PreferencesService } from 'src/app/services/common/preferences.service';
-import { WindowService } from 'src/app/services/common/window.service';
 
 @Component({
     selector: 'app-profile-code-dialog',
@@ -14,20 +13,15 @@ export class ProfileCodeDialog implements OnInit {
     protected profileUrl = signal('');
     protected isLightTheme = signal(false);
 
-    private userName = '';
-
     constructor(
         public dialogRef: MatDialogRef<ProfileCodeDialog>,
         public preferencesService: PreferencesService,
-        private windowService: WindowService,
         @Inject(MAT_DIALOG_DATA) public data?: string) {
     }
 
     ngOnInit(): void {
         if (this.data) {
-            this.userName = this.data;
-
-            this.profileUrl.set(this.windowService.getApplicationBaseUrl() + '/users/@' + this.userName);
+            this.profileUrl.set(this.data);
             this.isLightTheme.set(this.preferencesService.isLightTheme);
         }
     }
