54c776261649bcbb8876de79dc9918da1c15e02c
Marcin Czachurski <mczachurski@icloud.com>
2025-06-24 12:21:50 +0200

    Add input types to all inputs (prevent redirection via shortcuts)

--- src/app/services/common/focus-tracker.service.ts
+++ src/app/services/common/focus-tracker.service.ts
@@ -1,15 +1,22 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { BehaviorSubject } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class FocusTrackerService {
     public $isFocused = new BehaviorSubject<boolean>(false);
+    public dialog = inject(MatDialog)
 
     setFocusState(focused: boolean) {
         this.$isFocused.next(focused);
     }
 
     get isCurrentlyFocused(): boolean {
+        // If there are open dialogs, we consider the app focused.
+        if (this.dialog.openDialogs && this.dialog.openDialogs.length) {
+            return true;
+        }
+
         return this.$isFocused.value;
     }
 }
